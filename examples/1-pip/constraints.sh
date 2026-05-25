9fc5225ca5800893f76c011224a60c640a3c5ea3
CRSomosa <pipe2017.28@gmail.com>
2026-05-25 13:54:22 -0400

    Update: push local coursework to fork

--- examples/1-pip/constraints.sh
+++ examples/1-pip/constraints.sh
@@ -1,10 +1,11 @@
-AIRFLOW_VERSION=2.7.1
+#!/usr/bin/env bash
 
-# Extract the version of Python you have installed. If you're currently using Python 3.11 you may want to set this manually as noted above, Python 3.11 is not yet supported.
-PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+# Airflow and Python version
+export AIRFLOW_VERSION=3.0.2
+export PYTHON_VERSION=$(python3 --version | cut -d' ' -f2 | cut -d'.' -f1-2)
 
-CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
-# For example this would install 2.7.1 with python 3.8: https://raw.githubusercontent.com/apache/airflow/constraints-2.7.1/constraints-3.8.txt
+# Constraint URL for pip install
+export CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 
 echo "Install with the following command:"
-echo "pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}""
+echo "pip install apache-airflow==${AIRFLOW_VERSION} --constraint \"${CONSTRAINT_URL}\""
